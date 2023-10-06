5d3fcf077c454692d6fa45b104122dcbfa71a36c
Denis Costello <G00396309@atu.ie>
2023-10-06 13:27:42 +0100

    First commit

---
+++ src/main/java/ie/atu/lab3library/Lab3LibraryApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3LibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab3library/Lab3LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
