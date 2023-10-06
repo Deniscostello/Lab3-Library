ec7c85b1258bb428df7d416c2bfc99163f6d8452
Denis Costello <G00396309@atu.ie>
2023-10-06 14:08:25 +0100

    Created LibraryService

--- src/main/java/ie/atu/lab3library/LibraryService.java
+++ src/main/java/ie/atu/lab3library/LibraryService.java
@@ -1,4 +1,11 @@
 package ie.atu.lab3library;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class LibraryService {
+    private Book book;
+
 }
