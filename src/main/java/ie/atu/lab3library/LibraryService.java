0349d41f6206068d7d163ec45adcda18cce3f7e3
Denis Costello <G00396309@atu.ie>
2023-10-09 16:27:41 +0100

    Added addBook, had to add a suppress warning due to incorrect autowiring

--- src/main/java/ie/atu/lab3library/LibraryService.java
+++ src/main/java/ie/atu/lab3library/LibraryService.java
@@ -3,9 +3,19 @@ package ie.atu.lab3library;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 
 public class LibraryService {
-    private Book book;
+    private ArrayList <Book> books =new ArrayList<>();
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    public void addBook(Book book){
+        books.add(book);
+    }
+
+
 
 }
