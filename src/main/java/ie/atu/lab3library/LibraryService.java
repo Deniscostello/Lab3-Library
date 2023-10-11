70f19cf398a4a858bd1cf5f80bc466c8e541dd5a
Denis Costello <G00396309@atu.ie>
2023-10-11 09:16:55 +0100

    Handling incoming requests

--- src/main/java/ie/atu/lab3library/LibraryService.java
+++ src/main/java/ie/atu/lab3library/LibraryService.java
@@ -1,17 +1,18 @@
 package ie.atu.lab3library;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 @Service
+@Component
 
 public class LibraryService {
     private ArrayList <Book> books =new ArrayList<>();
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    @Autowired
+    @Autowired(required = false)
     public void addBook(Book book){
         books.add(book);
     }
